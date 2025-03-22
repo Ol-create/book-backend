f70b6b391f46de417ce10fbab7893625cf5ba53b
Ol-create <oluola96@gmail.com>
2025-03-22 15:39:13 +0100

    modify listener model for email comfirmation

--- models/listener.js
+++ models/listener.js
@@ -21,6 +21,14 @@ module.exports = (sequelize, DataTypes) => {
         type: DataTypes.STRING,
         allowNull: false,
       },
+      isVerified: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+      },
+      verificationToken: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
     },
     {
       sequelize,
